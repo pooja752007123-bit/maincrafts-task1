3f6900388d2766f84659bde29a95ea28cd3e34c4
pooja752007123-bit <pooja752007123@gmail.com>
2026-06-18 12:37:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function validateForm() {
 
+    let name = document.forms["contactForm"]["name"].value;
+    let email = document.forms["contactForm"]["email"].value;
+
+    if (name == "" || email == "") {
+        alert("Name and Email must be filled out!");
+        return false;
+    }
+
+    alert("Form submitted successfully!");
+    return true;
+}
